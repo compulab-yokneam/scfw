d6af7b70665296df5b3d1330fdd19bf0a3c76baa
Valentin Raevsky <valentin@compulab.co.il>
2021-07-06 08:14:57 +0300

    Add the source code
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>

---
+++ platform/main/build_info.h
@@ -0,0 +1,11 @@
+#ifndef BUILD_INFO_H
+#define BUILD_INFO_H
+
+#define SCFW_BRANCH imx_scfw_2021q1
+#define SCFW_BUILD 5168UL
+#define SCFW_COMMIT 0x875cfeb5UL
+#define SCFW_DATE "Mar 09 2021"
+#define SCFW_DATE2 Mar_09_2021
+#define SCFW_TIME "05:16:20"
+
+#endif
